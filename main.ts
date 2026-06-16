f1b23a4ede38b2657f0951818273fd3203992cef
IIIMADDINIII <martin@elend.name>
2026-06-16 11:42:31 +0200

    Deno does not use bin field using import.meta.main instead

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
+import { Command } from "@cliffy/command";
 import { join } from "@std/path";
 import { homedir } from "node:os";
+import packageJson from "./deno.json" with { type: "json" };
 
 /**
  * Patches all GitLens extensions found in the specified path.
@@ -22,4 +24,19 @@ export async function patchExtension(path: string): Promise<void> {
   const oldContent = await Deno.readTextFile(filePath);
   const newContent = oldContent.replace(/(async\s+visibility\s*\([^\)]\)\s*\{\s*)(if\s*\()/m, (_, prefix, suffix) => `${prefix}return "public";${suffix}`);
   await Deno.writeTextFile(filePath, newContent);
+}
+
+if (import.meta.main) {
+  await new Command()
+    .name("git-lens-tool")
+    .version(packageJson.version)
+    .description("A tool to patch the GitLens extension for Visual Studio Code.")
+    .option("-D, --vsCodeExtensionsDir=<path:string>", "Patch all GitLens extensions found in the specified path.", { conflicts: ["gitLensExtensionDir"] })
+    .option("-d, --gitLensExtensionDir=<path:string>", "Patch the GitLens extension found in the specified path.", { conflicts: ["vsCodeExtensionsDir"] })
+    .action(async (options, ..._args) => {
+      if (options.gitLensExtensionDir) return await patchExtension(options.gitLensExtensionDir);
+      if (options.vsCodeExtensionsDir) return await patchExtensions(options.vsCodeExtensionsDir);
+      return await patchExtensions();
+    })
+    .parse();
 }
